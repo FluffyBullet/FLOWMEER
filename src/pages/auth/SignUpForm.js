6fc2609e727c7fc1f1d7a9be9f047012f89bda50
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-05-30 21:37:48 +0100

    format signupform with styles

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -15,25 +15,37 @@ function SignUpForm() {
         </div>
         <div>
             <Container>
+                {/* Form layout imported from react bootstrap - edit details. */}
                 <Form>
-                    <Form.Group className="mb-3" controlId="username">
-                        <Form.Control type="text" placeholder="<-x Username" />
+                    {/* Requesting to create unique username */}
+                    <Form.Group className={` mb-3`} controlId="username" name="username">
+                        <Form.Label className="d-none">Username</Form.Label>
+                        <Form.Control className={`${styles.InputPlace}`} type="text" placeholder="<-x Username" />
                     </Form.Group>
                     <Row>
                         <Col>
-                            <Form.Group className="mb-3" controlId="first_name">
-                                <Form.Control type="text" placeholder="First Name" />
+                        {/* Asking for user name */}
+                            <Form.Group className={`mb-3 ${styles.first_name}`} controlId="FirstName" name="first_name">
+                                <Form.Label className="d-none">First Name</Form.Label>
+                                <Form.Control type="text" className={`${styles.InputPlace}`} placeholder="First Name" />
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="family_name">
-                                <Form.Control type="text" placeholder="Last Name" />
+                            <Form.Group className={`mb-3 ${styles.family_name}`} controlId="FamilyName" name="last_name">
+                                <Form.Label className="d-none">Last Name</Form.Label>
+                                <Form.Control type="text" className={`${styles.InputPlace}`} placeholder="Last Name" />
                             </Form.Group>
                         </Col>
                     </Row>
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Control type="password" placeholder="Create a password" />
+                    {/* Password section, original + authentication */}
+                    <Form.Group className="mb-3" controlId="password" name="password1">
+                        <Form.Label className="d-none">Password Entry</Form.Label>
+                        <Form.Control type="password" className={`${styles.InputPlace}`} placeholder="Create a password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="ConfirmPassword" name="password2">
+                        <Form.Label className="d-none">re-enter password</Form.Label>
+                        <Form.Control type="password" className={`${styles.InputPlace}`} placeholder="Confirm your password" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
-                        Submit
+                        Create account
                     </Button>
                 </Form>
         </Container>
