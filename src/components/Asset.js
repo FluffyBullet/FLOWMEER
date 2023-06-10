e74ae85bace88e24e7e93adee1f5838805a06688
FluffyBullet <davidreynolds34@hotmail.co.uk>
2023-06-10 08:05:09 +0000

    amend spinner to style from bootstrap

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -2,14 +2,10 @@ import React from "react";
 import styles from "../styles/Asset.module.css";
 import Spinner from 'react-bootstrap/Spinner';
 
-const Asset = ({Spinner, src, message}) => {
-    return (
-        <div className={`${styles.Asset} p-4`}>
-            {Spinner && <spinner animation="grow" variant="success" />}
-            {src && <img src={src} alt={message}/>}
-            {message && <p className="mt-4">{message}</p>}
-        </div>
-    );
+const Asset = () => {
+    return <>
+        <Spinner animation="grow" />
+        </>;
     };
 
 export default Asset;
